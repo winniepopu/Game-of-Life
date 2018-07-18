c72b6058467e6fd33585c1f0f4b724e7e66f78db
winniepopu <winniepopu@gmail.com>
2018-07-18 23:30:59 +0800

    complete

---
+++ main.py
@@ -0,0 +1,36 @@
+import gameoflife
+
+if __name__ == '__main__':
+    # You should not modify this part.
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--width',
+                       default='60',
+                       help='input the width of the map')
+    parser.add_argument('--height',
+                        default='23',
+                        help='input the height of the map')
+    parser.add_argument('--pattern',
+                        default='3',
+                        help='input pattern')
+    parser.add_argument('--generation',
+                        default='20',
+                        help='input generation')
+    args = parser.parse_args()
+
+    w = int(args.width)
+    h = int(args.height)
+    p = int(args.pattern)
+    t = int(args.generation)
+
+# w=50
+# h=40
+# p=2
+# t=5
+
+i=gameoflife.GameofLife(w,h)
+i.initialize(p)
+i.proceed(t)
+#i.gameoflife.display()
+
